733a8bee71d974db3db0d2b154944d8b5d45b67e
Alejandro Gironza <magironza@unicauca.edu.co>
2015-03-16 18:19:41 -0500

    primer trabajo de paso de pagina

---
+++ Libro/Libro/Models/libros.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libro.Models
+{
+    public  class Libros
+    {
+
+        private ObservableCollection<Libro> data;
+
+        public ObservableCollection<Libro> Data {
+            get
+            {
+                if (data == null) {
+                    data = new ObservableCollection<Libro>();
+                    Libro Libro1 = new Libro()
+                    {
+                        Nombre= "Padre ri",
+                        Autor="as",
+                        Codigo="dvr",
+                        Genero="comedia",
+                        Paginas=300
+
+                    };
+
+                    Libro Libro2 = new Libro()
+                    {
+                        Nombre = "Padre po",
+                        Autor = "by",
+                        Codigo = "dvre",
+                        Genero = "terror",
+                        Paginas = 150
+
+                    };
+
+                    Libro Libro3 = new Libro()
+                    {
+                        Nombre = "Padre me",
+                        Autor = "el",
+                        Codigo = "dvrr",
+                        Genero = "otaku",
+                        Paginas = 90
+
+                    };
+                    data.Add(Libro1);
+                    data.Add(Libro2);
+                    data.Add(Libro3);
+                   
+                
+                
+                }
+
+
+                return data;
+            
+            }
+
+            set {
+
+                data = value;
+            }
+        
+        }
+
+    }
+}
---
+++ Libro/Libro/PagLibros.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Libro.Models;
+
+namespace Libro
+{
+    public partial class PagLibros : PhoneApplicationPage
+    {
+        public PagLibros()
+        {
+            InitializeComponent();
+        }
+    
+
+     
+
+        private void gotopage(object sender, SelectionChangedEventArgs e)
+        {
+            var x = lista.SelectedIndex;
+            if (x == 0) {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=0", UriKind.Relative));
+            
+            }
+            if (x == 1)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=1",UriKind.Relative));
+
+            }
+            if (x == 2)
+            {
+                NavigationService.Navigate(new Uri("/MainPage.xaml?dato1=2", UriKind.Relative));
+
+            }
+            }
+        }
+
+
+
+
+
+}
